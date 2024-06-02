dd121c7ba2f9b05396b63d032afb65d082c472b3
franccostan <franccostan1233@gmail.com>
2024-06-02 18:27:17 +0800

    Update speech_recog.dart

--- lingomaster_final/lib/screens/speech_recog.dart
+++ lingomaster_final/lib/screens/speech_recog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart';
+import 'package:string_similarity/string_similarity.dart';
 
 class SpeechRecog extends StatefulWidget {
   const SpeechRecog({super.key});
@@ -14,6 +15,11 @@ class _SpeechRecogState extends State<SpeechRecog> {
   bool _speechEnabled = false;
   String _wordsSpoken = "";
   double _confidenceLevel = 0;
+  double _similarityScore = 0;
+
+  // temporary variable for the text to be compared
+  // modfy this later so it can be changed or fetched from another class
+  final String _targetText = "home";
 
   @override
   void initState() {
@@ -35,6 +41,8 @@ class _SpeechRecogState extends State<SpeechRecog> {
 
   void _stopListening() async {
     await _speechToText.stop();
+    _similarityScore = _wordsSpoken.similarityTo(_targetText);
+    print(_similarityScore);
     setState(() {});
   }
 
@@ -43,7 +51,7 @@ class _SpeechRecogState extends State<SpeechRecog> {
       _wordsSpoken = "${result.recognizedWords}";
       _confidenceLevel = result.confidence;
     });
-  }
+  } 
 
   @override
   Widget build(BuildContext context) {
@@ -60,6 +68,13 @@ class _SpeechRecogState extends State<SpeechRecog> {
       body: Center(
         child: Column(
           children: [
+            Text(
+                "Target Text: ${(_targetText)}",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w200,
+                ),
+              ),
             Container(
               padding: EdgeInsets.all(16),
               child: Text(
@@ -86,6 +101,14 @@ class _SpeechRecogState extends State<SpeechRecog> {
             if (_speechToText.isNotListening && _confidenceLevel > 0)
               Text(
                 "Confidence: ${(_confidenceLevel * 100).toStringAsFixed(1)}%",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w200,
+                ),
+              ),
+             if (_speechToText.isNotListening && _confidenceLevel > 0)
+              Text(
+                "Score: ${(_similarityScore * 100).toStringAsFixed(1)}%",
                 style: TextStyle(
                   fontSize: 30,
                   fontWeight: FontWeight.w200,
